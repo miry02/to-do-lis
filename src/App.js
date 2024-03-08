35510e824e3121d48247770c7ffe33065cd648fb
miry02 <92781397+miry02@users.noreply.github.com>
2024-03-08 21:33:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { TodoWrapper } from './components/TodoWrapper';
+import TaskForm from './components/TaskManagement';
+import TaskManagement from './components/TaskManagement';
 
 function App() {
   return (
     <div className="App">
       <TodoWrapper/>
+      <TaskManagement/>
     </div>
   );
 }
